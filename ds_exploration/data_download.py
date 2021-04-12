c72a5c7fa9a841c3bc9f8a13a857cb1382e26f60
Federico Calore <Federico.Calore@ing.nl>
2021-04-12 16:29:37 +0200

    Cleanup

--- ds_exploration/data_download.py
+++ ds_exploration/data_download.py
@@ -1,11 +1,16 @@
-from pathlib import Path
 import shutil
 import rasterio
 import fnmatch
+import os
+from pathlib import Path
 from glob import glob
 import numpy as np
 from ds_exploration.plotting_utils import plot_image
 
+os.chdir('../sentinelsat/')
+from sentinelsat import SentinelAPI
+from sentinelsat import SentinelProductsAPI
+os.chdir('../notebooks/')
 
 def make_filter(pattern):
     def node_filter(node_info, include_pattern=pattern):
@@ -31,12 +36,22 @@ def extract_and_combine(zip_file_path):
     blue = get_band_array("B02", band_files)
     return np.dstack([red, green, blue])
 
-def download_tiles(list_of_tiles, nfilter='*B??.jp2'):
+def download_tiles(list_of_tiles,
+                   output_folder,
+                   user,
+                   password,
+                   nfilter='*B??.jp2'
+                  ):
     if nfilter:
         nodefilter=make_filter(nfilter)
         api = SentinelProductsAPI(user, password, 'https://scihub.copernicus.eu/dhus')
     else:
         api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
+    
+    try:
+        os.mkdir(output_folder)
+    except:
+        pass
     
     for tile in list_of_tiles:
         print(f'Download tile name: {tile["tile_name"]} for {tile["date"][0]}, {tile["date"][1]}, with filter {nfilter}')
@@ -47,13 +62,13 @@ def download_tiles(list_of_tiles, nfilter='*B??.jp2'):
             platformname='Sentinel-2', producttype='S2MSI1C'
         )
         if nfilter:
-            downloaded_products = api.download_all(products, directory_path=dir_, n_concurrent_dl=5, nodefilter=nodefilter)
+            downloaded_products = api.download_all(products, directory_path=output_folder, n_concurrent_dl=5, nodefilter=nodefilter)
         else:
-            downloaded_products = api.download_all(products, directory_path=dir_, n_concurrent_dl=5)
+            downloaded_products = api.download_all(products, directory_path=output_folder, n_concurrent_dl=5)
         
         print(downloaded_products)
         filename = [downloaded_products[0][k]['title'] for k in downloaded_products[0].keys()][0]
-        zip_file_path = Path(f"{dir_}/{filename}.zip")
+        zip_file_path = Path(f"{output_folder}/{filename}.zip")
         unarchive_download(zip_file_path)
         
         rgb = extract_and_combine(zip_file_path)
