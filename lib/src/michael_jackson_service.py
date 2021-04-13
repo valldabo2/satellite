b85177561a0623736c5493a9e41c02daa52e731e
Utsav Jha <utsav.jha@ing.com>
2021-04-13 15:24:31 +0530

    Forceful commit 2

--- lib/src/michael_jackson_service.py
+++ lib/src/michael_jackson_service.py
@@ -4,8 +4,8 @@ from lib.src.data_fetch.data_download import download_tiles
 if __name__ == "__main__":
     list_of_tiles = [
         {"tile_name": "10SEH", "date": ("20200928", "20200930")},
-        # {"tile_name": "10TDK", "date": ("20201004", "20201009")},
-        # {"tile_name": "10TDL", "date": ("20200928", "20200930")},
+        {"tile_name": "10TDK", "date": ("20201004", "20201009")},
+        {"tile_name": "10TDL", "date": ("20200928", "20200930")},
     ]
 
     user = "utsavjha"
