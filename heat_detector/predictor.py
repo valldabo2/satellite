4c6d86e3cf1373242503536cd7be979c248291c8
mecton <chriolivier@gmail.com>
2021-04-14 12:18:32 +0100

    Fct doc

--- heat_detector/predictor.py
+++ heat_detector/predictor.py
@@ -3,11 +3,17 @@ import numpy as np
 
 class HeatPredictor(object):
     def predict(self, tiles, band_value_threshold=10_000, min_points=250, **kwargs):
+        """
+        :param tiles: list containing tiles data
+        :param band_value_threshold: B12 value we consider "hot enough"
+        :param min_points: minimum quantity of "hot enough" points to consider fire
+        :return: boolean, fire or not
+        """
         return [self._is_fire(t["B12"], band_value_threshold, min_points) for t in tiles]
 
     def _is_fire(self, b12, band_value_threshold=10_000, min_points=250):
         """
-        :param np_b12: np array containing the B12 band values
+        :param b12: list containing the B12 band values
         :param band_value_threshold: B12 value we consider "hot enough"
         :param min_points: minimum quantity of "hot enough" points to consider fire
         :return: boolean, fire or not
